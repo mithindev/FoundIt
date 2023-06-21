575b411958f796983fcd8684793106327c88bf20
mithindev <mithindev1@gmail.com>
2023-06-21 07:36:50 +0530

    updated

--- webcam-capture-0.3.12-dist/src/com/github/sarxos/webcam/WebcamDiscoveryListener.java
+++
@@ -1,9 +0,0 @@
-package com.github.sarxos.webcam;
-
-public interface WebcamDiscoveryListener {
-
-	void webcamFound(WebcamDiscoveryEvent event);
-
-	void webcamGone(WebcamDiscoveryEvent event);
-
-}
--- webcam-capture-0.3.12-dist/src/com/github/sarxos/webcam/util/Initializable.java
+++
@@ -1,9 +0,0 @@
-package com.github.sarxos.webcam.util;
-
-public interface Initializable {
-
-	void initialize();
-
-	void teardown();
-
-}
